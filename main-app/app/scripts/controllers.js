0de4c7d772e2ac53ee0f119e3f6dd40dc58d0dca
LewisGardner25 <lewis.gardner@tombola-0306.local>
2014-12-08 14:47:26 +0000

    new game button created and working

--- main-app/app/scripts/controllers.js
+++ main-app/app/scripts/controllers.js
@@ -1,7 +1,13 @@
-noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope){
+noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope,gameModel){
 
-    $scope.doSum = function () {
-        $scope.total = 10+10;
+    $scope.gameModel = gameModel;
+
+    $scope.newGame = function(){
+          callServer = ('http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame', {
+              'player1': gameModel.firstPlayer,
+              'player2': gameModel.secondPlayer
+          },
+       console.log('New Game Created'));
     };
 
     var callServer = {
--- main-app/app/scripts/gamemodel.js
+++ main-app/app/scripts/gamemodel.js
@@ -1,3 +1,11 @@
-noughtsAndCrossesApp.factory('gameModel', function($http){
+noughtsAndCrossesApp.service('gameModel', function(){
+
+    return {
+        gameboard: '000000000',
+        winner: 0,
+        firstPlayer: 'random',
+        secondPlayer: 'random',
+        outcome: 'continue'
+    };
 
 });
