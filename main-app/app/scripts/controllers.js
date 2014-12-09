f965c9e7d23b76a469598043c242583b29f708c5
LewisGardner25 <lewis.gardner@tombola-0306.local>
2014-12-09 11:32:18 +0000

    Added change player type button, can now change both player 1 and 2 to human, random or pre-trained

--- main-app/app/scripts/controllers.js
+++ main-app/app/scripts/controllers.js
@@ -1,14 +1,8 @@
-noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope,gameModel,$http){
+noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope,gameModel){
 
     $scope.gameModel = gameModel;
 
-    $scope.newGame = function(){
-          serverCall ('http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame', {
-              'player1': gameModel.player1,
-              'player2': gameModel.player2
-              },
-       console.log('New Game Created'));
-    };
+
 
     var serverCall = function (url, data) {
         var serverPost = {
@@ -20,4 +14,33 @@ noughtsAndCrossesApp.controller('noughtsAndCrossesController', function ($scope,
                 'content-type': 'application/json'
         }};
     };
+
+    $scope.newGame = function(){
+        serverCall ('http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame', {
+                'player1': gameModel.player1,
+                'player2': gameModel.player2
+            },
+            console.log('New Game Created'));
+    };
+
+    $scope.changePlayer1 = function () {
+        if (gameModel.player1 === 'random'){
+            gameModel.player1 = 'human';
+        } else if (gameModel.player1 === 'human'){
+            gameModel.player1 = 'pre-trained';
+        } else {
+            gameModel.player1 = 'random';
+        }
+    };
+
+    $scope.changePlayer2 = function () {
+        if (gameModel.player2 === 'random'){
+            gameModel.player2 = 'human';
+        } else if (gameModel.player2 === 'human'){
+            gameModel.player2 = 'pre-trained';
+        } else {
+            gameModel.player2 = 'random';
+        }
+    };
+
 });
