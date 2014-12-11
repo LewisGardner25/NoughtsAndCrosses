be9a417c265977d5912b07e88fe35f5e077524d7
LewisGardner25 <lewis.gardner@tombola-0306.local>
2014-12-11 16:26:37 +0000

    switchPlayer1 & 2 now working, had to be transferred to model folder

--- main-app/app/scripts/controllers.js
+++ main-app/app/scripts/controllers.js
@@ -1,19 +1,18 @@
-noughtsAndCrossesApp.controller('noughtsAndCrossesController',function ($scope,gameModel,gameApi,playerServices){
+noughtsAndCrossesApp.controller('noughtsAndCrossesController',function ($scope,gameModel,gameApi){
 
     $scope.gameModel = gameModel;
     $scope.gameApi = gameApi;
-    $scope.playerServices = playerServices;
 
     $scope.newGame = function(){
         gameApi.newGame();
     };
 
    $scope.switchPlayer1 = function(){
-        playerServices.switchPlayer1();
+        gameModel.switchPlayer1();
     };
 
     $scope.switchPlayer2 = function(){
-        playerServices.switchPlayer2();
+        gameModel.switchPlayer2();
     };
 
 });
--- main-app/app/scripts/gamemodel.js
+++ main-app/app/scripts/gamemodel.js
@@ -7,6 +7,26 @@ noughtsAndCrossesApp.service('gameModel', function(){
         this.player2 = 'random';
         this.outcome = 'continue';
         this.currentPlayer = 1;
+
+        var nextPlayerType = function (currentPlayer){
+            if(currentPlayer === 'random'){
+                return 'human';
+            }
+            if(currentPlayer === 'human'){
+                return 'pre-trained';
+            }
+            return 'random';
+        };
+
+        this.switchPlayer1 = function(currentPlayer){
+            var me = this;
+            me.player1 = nextPlayerType(me.player1);
+        };
+
+        this.switchPlayer2 = function(  ){
+            var me = this;
+            this.player2 = nextPlayerType(me.player2);
+        };
     };
     return new gameModel();
 });
