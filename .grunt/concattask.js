127e23ea5c76420f8b2adb1bf4a337393211c37c
LewisGardner25 <lewis.gardner@tombola-0306.local>
2015-01-15 09:30:23 +0000

    removed file and changed concatTask

--- .grunt/concattask.js
+++ .grunt/concattask.js
@@ -5,7 +5,10 @@ module.exports = {
     scripts: {
         src:
             [
-            'main-app/app/scripts/*.js'
+            'main-app/app/scripts/app.js',
+            'main-app/app/scripts/gameApi.js',
+            'main-app/app/scripts/gamemodel.js',
+            'main-app/app/scripts/controllers.js'
             ],
         dest: '.build/main-app/app/scripts/built.js'
     }
