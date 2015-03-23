e8cc961e75d0a8f5cb0244819468c2f4a2a71854
LewisGardner25 <lewis.gardner@tombola-0306.local>
2015-03-23 14:08:01 +0000

    added another test to game model tests and commented out game-api tests until I start working on them properly

--- unit-tests/game-api-tests.js
+++ unit-tests/game-api-tests.js
@@ -11,22 +11,22 @@
 //
 //    it('Should contain gameApi service', function(){
 //        expect(noughtsAndCrossesApp.gameApi)
-//            expect('noughtsAndCrossesApp.service(gameApi,function ($http,$q,gameApiConstants)')
+//            .to.have.property('noughtsAndCrossesApp.service(gameApi,function ($http,$q,gameApiConstants)')
 //    });
 //
 //    it('makeMove function contains appropriate playerNumber and squareNumber', function(){
 //        expect(noughtsAndCrossesApp.gameApi.makeMove)
-//            expect(gameApi.makeMove.playerNumber = 'playerNumber',gameApi.makeMove.squareNumber = 'squareNumber');
+//            .to.have.property(gameApi.makeMove.playerNumber = 'playerNumber',gameApi.makeMove.squareNumber = 'squareNumber');
 //    });
 //
 //    it('newGame function contains constants', function(){
 //        expect(noughtsAndCrossesApp.gameApi.newGame)
-//            expect(gameApi.newGame.player1 = 'player1Type',gameApi.newGame.player2 = 'player2Type');
+//            .to.have.property(gameApi.newGame.player1 = 'player1Type',gameApi.newGame.player2 = 'player2Type');
 //    });
 //
 //    it('gameApi service contains promise', function(){
 //        expect(noughtsAndCrossesApp.gameApi)
-//            expect(gameApi.to.have.property('serverCall'));
+//            .to.have.property(gameApi.to.have.property('serverCall'));
 //    });
 //
 //});
--- unit-tests/game-model-tests.js
+++ unit-tests/game-model-tests.js
@@ -12,10 +12,15 @@
         });
 
         it('Checks if a new game has been created',function(){
-            expect(gameModel.gameboard).to.equal('000000000')
+            expect(gameModel.gameboard).to.equal('000000000');
         });
 
+        it('player1 and player2 are set to human upon beginning a new game',function(){
+            expect(gameModel.player1).to.equal('human');
+            expect(gameModel.player2).to.equal('human');
+        });
 
+        it()
 
     });
 }());
