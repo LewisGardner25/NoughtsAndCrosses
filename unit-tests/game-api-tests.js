7de48bc8085de49cd938493a91d0b4c156081b47
LewisGardner25 <lewis.gardner@tombola-0306.local>
2015-04-22 15:32:44 +0100

    added two tests to gameApi testing the newGame function and makeMove function

--- unit-tests/game-api-tests.js
+++ unit-tests/game-api-tests.js
@@ -1,32 +1,24 @@
-//describe('gameApiTest', function(){
-//
-//    var gameApi;
-//
-//    beforeEach(function(){
-//        module('tombola.noughtAndCrosses.services');
-//        inject(function($injector){
-//            gameApi = $injector.get('gameApi');
-//        });
-//    });
-//
-//    it('Should contain gameApi service', function(){
-//        expect(noughtsAndCrossesApp.gameApi)
-//            .to.equal('noughtsAndCrossesApp.service(gameApi,function ($http,$q,gameApiConstants)')
-//    });
-//
-//    it('makeMove function contains appropriate playerNumber and squareNumber', function(){
-//        expect(noughtsAndCrossesApp.gameApi.makeMove)
-//            .to.equal(gameApi.makeMove.playerNumber = 'playerNumber',gameApi.makeMove.squareNumber = 'squareNumber');
-//    });
-//
-//    it('newGame function contains constants', function(){
-//        expect(noughtsAndCrossesApp.gameApi.newGame)
-//            .to.equal(gameApi.newGame.player1 = 'player1Type',gameApi.newGame.player2 = 'player2Type');
-//    });
-//
-//    it('gameApi service contains promise', function(){
-//        expect(noughtsAndCrossesApp.gameApi)
-//            .to.equal(gameApi.callServer);
-//    });
-//
-//});
+describe('gameApiTest', function(){
+
+    var gameApi;
+    var gameModel;
+
+    beforeEach(function(){
+        module('tombola.noughtAndCrosses.services');
+        inject(function($injector){
+            gameApi = $injector.get('gameApi');
+            gameModel = $injector.get('gameModel')
+        });
+    });
+
+    it('newGame function should reset the game board', function(){
+        gameApi.newGame();
+        expect(gameModel.gameboard).to.equal('000000000')
+    });
+
+    it('makeMove function finds currentPlayer and squareNumber placement',function(){
+        gameApi.makeMove();
+        expect(gameModel.currentPlayer).to.equal(1||2);
+    });
+
+});
